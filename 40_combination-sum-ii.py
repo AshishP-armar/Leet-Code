bbda23d2a13ec1348565362afb94cf153002355f
ASHISH PARMAR <165138959+AshishP-armar@users.noreply.github.com>
2024-09-25 10:02:53 +0530

    Update 40_combination-sum-ii.py
    Solution Add

--- 40_combination-sum-ii.py
+++ 40_combination-sum-ii.py
@@ -7,3 +7,23 @@ Each number in candidates may only be used once in the combination.
 
 Note: The solution set must not contain duplicate combinations.
 """
+
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        def backtrack(start: int, target: int, path: List[int]):
+            if target == 0:
+                result.append(path)
+                return
+            for i in range(start, len(candidates)):
+               
+                if i > start and candidates[i] == candidates[i - 1]:
+                    continue
+                if candidates[i] > target:
+                    break
+                
+                backtrack(i + 1, target - candidates[i], path + [candidates[i]])
+
+        result = []
+        candidates.sort()  
+        backtrack(0, target, [])
+        return result
