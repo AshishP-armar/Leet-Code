b092ecd8539fdcffeceedd5948704a306c62d128
ASHISH PARMAR <165138959+AshishP-armar@users.noreply.github.com>
2024-09-26 10:18:06 +0530

    Update 2446.py
    test cases add

--- 2446.py
+++ 2446.py
@@ -9,4 +9,21 @@ A conflict happens when two events have some non-empty intersection (i.e., some 
 
 Return true if there is a conflict between two events. Otherwise, return false.
 
+test case :- 
+Example 1:
+
+Input: event1 = ["01:15","02:00"], event2 = ["02:00","03:00"]
+Output: true
+Explanation: The two events intersect at time 2:00.
+Example 2:
+
+Input: event1 = ["01:00","02:00"], event2 = ["01:20","03:00"]
+Output: true
+Explanation: The two events intersect starting from 01:20 to 02:00.
+Example 3:
+
+Input: event1 = ["10:00","11:00"], event2 = ["14:00","15:00"]
+Output: false
+Explanation: The two events do not intersect.
+
 '''
