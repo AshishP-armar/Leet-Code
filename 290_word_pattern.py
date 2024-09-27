42a5701ea3b07ce4f3f4b616b8470f876233e6fc
ASHISH PARMAR <165138959+AshishP-armar@users.noreply.github.com>
2024-09-27 10:02:39 +0530

    Update 290_word_pattern.py
    test case add

--- 290_word_pattern.py
+++ 290_word_pattern.py
@@ -9,3 +9,31 @@ Each letter in pattern maps to exactly one unique word in s.
 Each unique word in s maps to exactly one letter in pattern.
 No two letters map to the same word, and no two words map to the same letter.
 """
+
+# test cases
+"""
+Example 1:
+
+Input: pattern = "abba", s = "dog cat cat dog"
+
+Output: true
+
+Explanation:
+
+The bijection can be established as:
+
+'a' maps to "dog".
+'b' maps to "cat".
+Example 2:
+
+Input: pattern = "abba", s = "dog cat cat fish"
+
+Output: false
+
+Example 3:
+
+Input: pattern = "aaaa", s = "dog cat cat dog"
+
+Output: false
+"""
+
