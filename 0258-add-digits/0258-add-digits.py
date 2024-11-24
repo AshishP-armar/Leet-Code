a2fa651c3ad610df7dfc2a0b27e9be7108785558
ASHISH PARMAR <165138959+AshishP-armar@users.noreply.github.com>
2024-11-24 10:45:38 +0530

    Time: 0 ms (100%), Space: 16.9 MB (7.97%) - LeetHub

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -2,7 +2,6 @@ class Solution:
     def addDigits(self, num: int) -> int:
         if len(str(num))==1:
             return num
-
         while len(str(num))!=1:
             num = self.digitSum(num)
         return num
